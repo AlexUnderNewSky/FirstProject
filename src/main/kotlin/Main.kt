2e1c1d2ea4875c68ac2899700681b220cdc6c001
AlexUnderNewSky <Sergio380505056123>
2023-06-14 13:39:19 +0300

    1 lesson done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,58 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val text: String = "4"
+    val a: Int = 25
+    val txt: String = a.toString() + " Yeah!"
+    val sum: Int = text.toInt()+10
+    println(txt)
+    println(sum)
 }
+
+/*
+    var a: Int = 5
+    var name: String = "rectangle"
+    var diagonal: Double = 5.83
+    var isSquare: Boolean = false
+*/
+
+
+/*//input value A
+    print("Enter A: ")
+    val a: Int = readln().toInt()
+    println("A = $a.")
+
+    //input value B
+    print("Enter B: ")
+    val b: Int = readln().toInt()
+    println("B = $b.")
+
+    //data processing
+    val sum: Int = a + b
+
+    //data output
+    println("Sum of A and B = $sum!")*/
+
+
+    /*Переменные всегда с маленькой буквы!
+    val salary: Int = 3400
+
+    val userName: String = "Kirill"
+    println(salary)*/
+
+    /*var text: String = "Hello"
+
+    text += " World!"
+
+    val letter: Char = 't'
+    println(letter)*/
+
+
+    /*
+    val name: String = "Kirill"
+    val age: Int = 27
+    val document: String = "passport"
+
+    //ticket buy
+
+    //passenger Kirill, age: 27, document type: passport
+
+    println("Name: $name | Age: $age | Document type: $document")*/
