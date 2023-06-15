48280b39116b9de9ffce0b4cc08232afc046277a
AlexUnderNewSky <Sergio380505056123>
2023-06-15 13:40:46 +0300

    2 lesson done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,28 @@
 fun main() {
-    val text: String = "4"
-    val a: Int = 25
-    val txt: String = a.toString() + " Yeah!"
-    val sum: Int = text.toInt()+10
-    println(txt)
-    println(sum)
+
+    print("Please enter your rating: ")
+    val rating: Int = readln().toInt()
+
+//    for 1 will be - bad
+//    for 2 will be - bad
+//    for 3 will be - normal
+//    for 4 will be - good
+//    for 5 will be - excellent
+
+
+    /*val answer: String = when {
+        rating == 1 || rating == 2 -> "Bad = $rating"
+        rating == 3 -> "Normal = $rating"
+        rating == 4 -> "Good = $rating"
+        rating > 4 && rating <= 10 -> "Excellent = $rating"
+        else -> "Insane = $rating"
+    }
+    println(answer)*/
+
+    val condition1: Boolean = rating == 1 || rating == 2
+
+    println(condition1)
+
 }
 
 /*
@@ -32,27 +50,61 @@ fun main() {
     println("Sum of A and B = $sum!")*/
 
 
-    /*Переменные всегда с маленькой буквы!
-    val salary: Int = 3400
+/*Переменные всегда с маленькой буквы!
+val salary: Int = 3400
 
-    val userName: String = "Kirill"
-    println(salary)*/
+val userName: String = "Kirill"
+println(salary)*/
 
-    /*var text: String = "Hello"
+/*var text: String = "Hello"
 
-    text += " World!"
+text += " World!"
 
-    val letter: Char = 't'
-    println(letter)*/
+val letter: Char = 't'
+println(letter)*/
 
 
-    /*
-    val name: String = "Kirill"
-    val age: Int = 27
-    val document: String = "passport"
+/*
+val name: String = "Kirill"
+val age: Int = 27
+val document: String = "passport"
 
-    //ticket buy
+//ticket buy
 
-    //passenger Kirill, age: 27, document type: passport
+//passenger Kirill, age: 27, document type: passport
 
-    println("Name: $name | Age: $age | Document type: $document")*/
+println("Name: $name | Age: $age | Document type: $document")*/
+
+/*val text: String = "4"
+val a: Int = 25
+val txt: String = a.toString() + " Yeah!"
+val sum: Int = text.toInt()+10
+println(txt)
+println(sum)*/
+
+
+/*
+//    if age of passenger > 14, then need a passport, otherwise need birth certificate
+
+    val passengerAge: Int = 15
+
+    val document = if (passengerAge > 14)
+        "passport"
+    else
+        "birth certificate"
+
+    println(document)
+*/
+
+/*    val number1: Int = 32
+    val number2: Int = 23
+    val number3: Int = 68
+
+    val answer = if(number1 > number2 && number1 > number3)
+        "number 1 is bigger - $number1"
+    else if (number2 > number1 && number2 > number3)
+        "number 2 is bigger - $number2"
+    else
+        "number 3 is bigger - $number3"
+
+    println(answer)*/
